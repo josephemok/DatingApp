acfcad1538ee86bf023f94c6175b8003e66ff798
Joseph Emok <josephemok@live.com>
2021-01-20 12:34:26 +0100

    end section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
+
+
 
 @Component({
   selector: 'app-root',
@@ -8,19 +12,16 @@ import {HttpClient} from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   title = 'The Dating App';
-  constructor(private http: HttpClient) {}
   users: any;
+
+  constructor(private accountService: AccountService) {}
+  
   ngOnInit() {
-   this.getUsers();
+     this.setCurrentUser();
   }
 
-  getUsers(){
-
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-     this.users = response;
-
-   }, error => {
-     console.log(error);
-   })
+  setCurrentUser(){
+    const user: User =JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
-}
+ }
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+ // @Input() usersFromHomeComponent: any;
+  @Output() cancelRegister = new EventEmitter();
+  model: any ={};
+
+  constructor(private accountServivce: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  register(){
+    this.accountServivce.register(this.model).subscribe(response => {
+      console.log(response);
+      this.cancel();
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  cancel() {
+    //console.log('cancelled');
+    this.cancelRegister.emit(false);
+  }
+
+}
