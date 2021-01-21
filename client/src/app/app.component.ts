ab7507f0ec4f2aa80d415c56612e5fdcc0f69021
Joseph Emok <josephemok@live.com>
2021-01-21 20:36:35 +0100

    end of section 6

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 
 
