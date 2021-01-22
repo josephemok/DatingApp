0971d2bb9d0cab076c6e24f9500dbd5f6085bce9
Joseph Emok <josephemok@live.com>
2021-01-23 00:33:01 +0100

    end of section 7

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,6 +18,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using API.Extension;
+using API.Middleware;
 
 namespace API
 {
@@ -44,10 +45,12 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+          /*  if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
+            }*/
+            
+            app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseHttpsRedirection();
 
